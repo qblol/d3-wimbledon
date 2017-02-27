37948738c590dc6453cddb8032e3dfaf616f5165
Muhammad Iqbal <iqbal.0809@gmail.com>
2017-02-27 12:48:39 +0700

    done scaling

--- script.js
+++ script.js
@@ -14,13 +14,54 @@ let svg = d3.select('#results')
 
 // Data reloading
 let reload = () => {
-  // Your data parsing here...
+  let data = []
+  d3.tsv("afcw-results.tsv", function(rows) {
+    rows.forEach(function(goal){
+      data.push(goal.GoalsScored)
+    })
+    console.log(data);
+    redraw(data)
+  });
 }
 
 // redraw function
 let redraw = (data) => {
   // Your data to graph here
 
+  let temp = data.map((item)=>{
+    return parseInt(item)
+  })
+
+  let highest = Math.max(...temp)
+
+  const yScale = d3.scaleLinear()
+    .domain([0,highest])
+    .range([0,height])
+
+  const xScale = d3.scaleLinear()
+    .domain([0,data.length])
+    .range([0,width])
+
+  // let m = height / 4
+  svg.style('background', '#cacaca')
+     .selectAll('rect')
+     .data(data)
+     .enter()
+     .append('rect')
+     .attr('class', 'bar')
+     .attr('x', (d, i) => {
+       return xScale(i)
+     })
+     .attr('y', (d) => {
+       return height - yScale(d)
+     })
+     .attr('width', (i) => {
+      //  return xScale(i) - 2
+      return width/data.length - 2
+     })
+     .attr('height', (d)  => {
+       return yScale(d)
+     })
 }
 
 reload()
